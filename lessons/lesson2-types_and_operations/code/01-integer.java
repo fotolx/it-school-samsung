26c95c4a01a582eddd009f229a03f9ab7f43d3ed
haschish <haschish@gmail.com>
2019-09-23 13:51:42 +0500

    lesson1: added some code and presentation-dmitry

---
+++ lessons/lesson2-types_and_operations/code/01-integer.java
@@ -0,0 +1,24 @@
+public class Main {
+  public static void main(String[] args) {
+    byte b1 = 10;
+    short s1 = 5_000;
+    System.out.println(b1);
+    System.out.println(s1);
+    System.out.println();
+
+    int i1;
+    int i2 = 5;
+    int i3 = 100_500;
+    // System.out.println(i1);
+    System.out.println(i2);
+    System.out.println(i3);
+    System.out.println();
+
+    long l1 = 100L;
+    long l2 = 100l;
+    long l3 = 100;
+    System.out.println(l1);
+    System.out.println(l2);
+    System.out.println(l3);
+  }
+}
