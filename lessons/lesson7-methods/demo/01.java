931f87849a6b203a931eb6db5af118cb34083568
haschish <haschish@gmail.com>
2019-10-14 14:37:49 +0500

    lesson7: changed demo

--- lessons/lesson7-methods/demo/01.java
+++ lessons/lesson7-methods/demo/01.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-      int[] arr1 = new int[] {1, 2, 3, 4, 5};
+      int[] arr1 = new int[] {1, 2, 5, 4, 3};
       int[] arr2 = new int[] {6, 7, 8, 9, 10};
       int[] arr3 = new int[] {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
       printArray(arr1);
       printArray(arr2);
       printArray(arr3);
+      System.out.println();
+
+      System.out.println("max value of arr1 - " + getMax(arr1));
     }
 
     public static void printArray(int[] arr) {
@@ -14,5 +17,16 @@ public class Main {
           System.out.print(arr[i] + " ");
       }
       System.out.println();
+    }
+
+    public static int getMax(int[] arr) {
+      int max = arr[0];
+      for (int i: arr) {
+        if (i > max) {
+          max = i;
+        }
+      }
+
+      return max;
     }
 }
